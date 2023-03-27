1d1272c2eec0ca3a430c2edbd44dd966018f1640
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-03-27 21:46:28 +1300

    created a filter for options not availabe in the model

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,10 +1,22 @@
+const Product = require('../models/productModel')
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'product testing route'})
+    const products = await Product.find({ 
+        // name: 'vase table',
+        // featured: true,
+    })
+    res.status(200).json({ products, numberOfHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'product route'})
+    const {featured} = req.query
+    const queryObject = {}
+    if(featured) {
+        queryObject.featured = featured ==='true' ? true : false
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products, numberOfHits: products.length })
 }
 
 module.exports = {
